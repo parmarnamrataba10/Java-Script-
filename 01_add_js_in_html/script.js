e2aceeda79bb2b67302b2964ff517d20ae9731b9
parmarnamrataba10 <parmarnamrataba0@gmail.com>
2026-07-06 23:35:21 +0530

    js_in_html

---
+++ 01_add_js_in_html/script.js
@@ -0,0 +1,8 @@
+
+ document.getElementById("heading")
+
+        document.getElementById("btn")
+            .addEventListener("click", function myFunction() {
+
+                document.getElementById("heading").innerHTML = "welcome ";
+            });
